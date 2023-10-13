edc62693ef7d85b843a3bb80184a48f944eb477d
kylie hubbard <119622216+kyliehubbard@users.noreply.github.com>
2023-10-13 10:46:59 -0400

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -1,8 +1,5 @@
 /*
 CISC-124 2023 Summer
-Name: Kylie Hubbard
-Student Number: 20294570
-Email: kylie.hubbard@queensu.ca
 Date: 2023-08-01
 I confirm that this assignment solution is my own work and conforms to Queen’s
 standards of Academic Integrity.
